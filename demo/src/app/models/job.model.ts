2c77d57218603d165d80cccbad73227b4710bd75
Jogboms <jeremiahogbomo@gmail.com>
2017-09-20 20:02:51 +0100

    chore(demo):  setup Test Model

---
+++ demo/src/app/models/job.model.ts
@@ -0,0 +1,7 @@
+import { SailsModel } from "ngx-sails-socketio";
+import { Endpoint } from "ngx-sails-socketio/src/sails.decorator.endpoint";
+
+@Endpoint("verification")
+export class JobModel extends SailsModel {
+
+}
