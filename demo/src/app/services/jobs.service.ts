59b6192b756c6b85b4eb73b4dedb33beb2e30360
Jogboms <jeremiahogbomo@gmail.com>
2017-09-23 21:12:20 +0100

    chore(demo): update

--- demo/src/app/services/jobs.service.ts
+++ demo/src/app/services/jobs.service.ts
@@ -11,21 +11,12 @@ export class JobsService {
     }
 
     getActiveJobs(): Promise<BoqModel[]> {
-        // const query = new SailsQuery<BoqModel>(this.sails, BoqModel);
         const req = new SailsRequest(this.sails);
         const criteria = (new RequestCriteria());
-        // .whereContains("status", "pending");
-        // criteria.whereLessThan("createdAt", new Date)
-        //     .or()
-        //     .whereLessThanOrEqualTo("createdAt", new Date);
-
-        // query.setRequestCriteria(criteria);
-        // query.setPopulation("customer");
-        // query.setLimit(25);
-        // return query.find();
-        // req.addParam("body", true);
+        req.addParam("where", criteria.build());
 
         return new Promise<BoqModel[]>((resolve, reject) => {
+            // req.get("/boq", (response) => {
             req.get("/boq", (response) => {
                 if (response.getStatusCode() === 200) {
                     return resolve(response.getBody().data);
@@ -38,5 +29,18 @@ export class JobsService {
     getNoCriteria() {
         const query = new SailsQuery<JobModel>(this.sails, JobModel);
         query.find();
+    }
+
+    getBoqs() {
+        const query = new SailsQuery<BoqModel>(this.sails, BoqModel);
+
+        const criteria = (new RequestCriteria())
+            .whereContains("status", "pending")
+            .whereLessThan("createdAt", new Date)
+            .or()
+            .whereLessThanOrEqualTo("createdAt", new Date);
+
+        query.setRequestCriteria(criteria).setPopulation("customer").setLimit(25);
+        return query.find();
     }
 }
