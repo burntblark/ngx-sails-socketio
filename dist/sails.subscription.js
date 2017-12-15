0febe2f34eacbcdd4e743c456a36a444a998dd57
Familusi Babatunde <burntblark@gmail.com>
2017-12-15 16:00:19 +0100

    chore: Added new interfaces to barrel

---
+++ dist/sails.subscription.js
@@ -0,0 +1,13 @@
+var SailsSubscription = /** @class */ (function () {
+    function SailsSubscription(sails) {
+        this.sails = sails;
+    }
+    SailsSubscription.prototype.on = function (eventName) {
+        return this.sails.on(eventName.toLowerCase());
+    };
+    SailsSubscription.prototype.off = function (eventName) {
+        return this.sails.off(eventName.toLowerCase());
+    };
+    return SailsSubscription;
+}());
+export { SailsSubscription };
