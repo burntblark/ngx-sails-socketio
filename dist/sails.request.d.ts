218e812e1a8fb0c7431715047c456d1e7f36bcb0
Jogboms <jeremiahogbomo@gmail.com>
2017-12-18 12:42:42 +0100

    build: angular distribution files

--- dist/sails.request.d.ts
+++ dist/sails.request.d.ts
@@ -1,6 +1,7 @@
 import { Sails } from "./sails";
 import { SailsResponse } from "./sails.response";
 import { SailsIOClient } from "./sails.io.client";
+import { Observable } from "rxjs/Observable";
 export declare const Method: {
     GET: string;
     POST: string;
@@ -12,11 +13,11 @@ export declare class SailsRequest {
     private sails;
     private parameters;
     constructor(sails: Sails);
-    get(url: string, headers?: SailsIOClient.Headers): Promise<SailsResponse>;
-    post(url: string, params: object, headers?: SailsIOClient.Headers): Promise<SailsResponse>;
-    put(url: string, params: object, headers?: SailsIOClient.Headers): Promise<SailsResponse>;
-    delete(url: string, headers?: SailsIOClient.Headers): Promise<SailsResponse>;
-    patch(url: string, headers?: SailsIOClient.Headers): Promise<SailsResponse>;
+    get(url: string, headers?: SailsIOClient.Headers): Observable<SailsResponse>;
+    post(url: string, params: object, headers?: SailsIOClient.Headers): Observable<SailsResponse>;
+    put(url: string, params: object, headers?: SailsIOClient.Headers): Observable<SailsResponse>;
+    delete(url: string, headers?: SailsIOClient.Headers): Observable<SailsResponse>;
+    patch(url: string, headers?: SailsIOClient.Headers): Observable<SailsResponse>;
     private _request(method, url, params?, headers?);
     addParam(name: string, value: boolean | number | string | {
         toString(): string;
