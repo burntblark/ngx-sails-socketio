7472e71ce16d8b3a8990a1c9fc7fceb7f6ad1b09
Jogboms <jeremiahogbomo@gmail.com>
2017-12-18 20:24:56 +0100

    chore(demo): code clean up

--- demo/src/app/components/dashboard/dashboard.component.ts
+++ demo/src/app/components/dashboard/dashboard.component.ts
@@ -3,10 +3,10 @@ import { Router } from "@angular/router";
 import { Sails } from "ngx-sails-socketio";
 import { JobsService } from "../../services/jobs.service";
 import "rxjs/add/operator/catch";
-import "rxjs/add/operator/withLatestFrom";
 import "rxjs/add/observable/race";
-import "rxjs/add/observable/merge";
+import "rxjs/add/operator/do";
 import { Observable } from "rxjs/Observable";
+import { BoqModel } from "../../models/boq.model";
 
 @Component({
     selector: "app-dashboard",
@@ -20,35 +20,10 @@ export class DashboardComponent implements OnInit {
     constructor(private router: Router, private sails: Sails, private jobs: JobsService) { }
 
     ngOnInit() {
-        // this.jobs.getJobs()
-        // this.jobs.getQueried()
-        //     // this.jobs.getJobs()
-        //     // this.jobs.getBoqs()
-        //     .catch(e => {
-        //         console.log(e);
-        //         return [];
-        //     })
-        //     .subscribe(data => {
-        //         console.log(data);
-
-        //         // const model = data[0];
-        //         // if (model) {
-        //         //     // this.jobs.updateJob(model).catch(e => console.log(e));
-        //         //     this.jobs.saveBoq(model).catch(e => console.log(e));
-        //         // }
-        //     });
-
-        // Pub-Sub
-        // this.jobs.listenOne().subscribe(info => console.log(info));
-        this.boqs$ = Observable.merge(this.jobs.getQueried(), this.jobs.listenAll());
-        // this.boqs$ = this.jobs.getQueried();
-        // this.boqs$ = Observable.race(this.jobs.getQueried());
-        // this.jobs.listenAll()
-        //     .withLatestFrom(this.jobs.getQueried())
-        // .
-        // ;
-        // .subscribe(info => console.log(info));
-        // this.jobs.listen();
+        this.boqs$ = Observable.race<BoqModel[]>(
+            this.jobs.listenAll().do(() => console.log("Second")),
+            this.jobs.getQueried().do(() => console.log("First")),
+        );
     }
 
     onLogout(e: Event) {
